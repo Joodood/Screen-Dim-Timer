3df5539d1a14d6be188a0cbe633ab883f030d53b
Jude Hathway <jude.hathway@gmail.com>
2022-02-19 15:54:07 -0500

    new version practice_run.py successfully dims your screen back to default if you choose to exit the application during any moment of the application's life cycle.

---
+++ practice_run.py
@@ -0,0 +1,195 @@
+import screen_brightness_control as pct
+import time
+import threading
+import tkinter as tk
+from tkinter import ttk, PhotoImage
+
+class Hun():
+    def __init__(self, current_b, dim):
+        self.root = tk.Tk()
+        self.root.geometry("600x300")
+        self.root.title("Timer")
+        self.root.tk.call('wm', 'iconphoto', self.root._w, PhotoImage(file="area.png"))
+
+        self.print_me_once = True
+
+        self.current_b = current_b
+        self.dim = dim
+
+        self.pct = pct
+        # self.current_b = pct.get_brightness()
+        # self.current_bright = self.pct.get_brightness()
+
+        self.s = ttk.Style()
+        self.s.configure("TNotebook.Tab", font = ("Ubuntu", 16))
+        self.s.configure("TButton", font=("Ubuntu", 16))
+
+        self.tabs = ttk.Notebook(self.root)
+        self.tabs.pack(fill = "both", pady=10, expand = True)
+
+        self.tab1 = ttk.Frame(self.tabs, width=600, height=100)
+        self.tab2 = ttk.Frame(self.tabs, width=600, height=100)
+        self.tab3 = ttk.Frame(self.tabs, width=600, height=100)
+
+        self.pomodoro_timer_label = ttk.Label(self.tab1, text = "25:00", font = ("Ubuntu", 48))
+        self.pomodoro_timer_label.pack(pady = 20)
+
+        self.short_break_timer_label = ttk.Label(self.tab2, text = "05:00", font = ("Ubuntu", 48))
+        self.short_break_timer_label.pack(pady = 20)
+
+        self.long_break_timer_label = ttk.Label(self.tab3, text = "15:00", font = ("Ubuntu", 48))
+        self.long_break_timer_label.pack(pady = 20)
+
+
+        self.tabs.add(self.tab1, text = "Pomodoro")
+        self.tabs.add(self.tab2, text = "Short Break")
+        self.tabs.add(self.tab3, text = "Long Break")
+
+
+        self.grid_layout = ttk.Frame(self.root)
+        self.grid_layout.pack(pady=10)
+
+        self.start_button = ttk.Button(self.grid_layout, text = "Start", command = self.start_timer_thread)
+        self.start_button.grid(row = 0, column = 0)
+
+        self.skip_button = ttk.Button(self.grid_layout, text = "Skip", command = self.skip_clock)
+        self.skip_button.grid(row = 0, column = 1)
+
+        self.reset_button = ttk.Button(self.grid_layout, text = "Reset", command = self.reset_clock)
+        self.reset_button.grid(row = 0, column = 2)
+
+        self.pomodoro_counter_label = ttk.Label(self.grid_layout, text = "Pomodoros: 0", font = ("Ubuntu", 16))
+        self.pomodoro_counter_label.grid(row=1, column=0,columnspan=3, pady=40)
+
+        self.pomodoros = 0
+        self.skipped = False
+        self.stopped = False
+        self.running = False
+
+        #make sure when user quits that the brightness is changed back to normal
+
+        self.root.protocol('WM_DELETE_WINDOW', self.on_closing)
+        self.root.mainloop()
+
+    def on_closing(self):
+        self.pct.set_brightness(self.current_b)
+        self.root.destroy()
+
+
+    def start_timer_thread(self):
+        if not self.running:
+            t = threading.Thread(target=self.start_timer)
+            t.start()
+            self.running = True
+
+    def start_timer(self):
+        #see which tab is selected, and then depending on that you run a different timer
+        #see what tab is selected
+        self.stopped = False
+        self.skipped = False
+        timer_id = self.tabs.index(self.tabs.select()) + 1
+        #will result in 0 1 and 2
+
+        if timer_id == 1:
+
+            # full_seconds = 60 * 25
+            full_seconds = 5
+            while full_seconds > 0 and not self.stopped:
+                minutes, seconds = divmod(full_seconds, 60)
+                self.pomodoro_timer_label.config(text = f"{minutes:02d}: {seconds:02d}")
+                self.root.update()
+                time.sleep(1)
+                full_seconds = full_seconds - 1
+            #if loop not stopped by button, very good
+            # or stop is true and skip is true
+            #if the time passes, or we skip a loop, we want to count up on our pomodoro
+            if not self.stopped or self.skipped:
+                self.pomodoros += 1
+                self.pomodoro_counter_label.config(text=f"Pomodoros: {self.pomodoros}")
+                if self.pomodoros % 4 == 0:
+                    self.tabs.select(2)
+                    self.start_timer()
+                else:
+                    self.tabs.select(1)
+                    self.start_timer()
+
+        elif timer_id == 2:
+            # full_seconds = 60 * 5
+            full_seconds = 5
+            while full_seconds > 0 and not self.stopped:
+                minutes, seconds = divmod(full_seconds, 60)
+                self.short_break_timer_label.config(text = f"{minutes:02d}: {seconds:02d}")
+                self.root.update()
+                time.sleep(1)
+
+                # self.brightness.set_brightness(1)
+                # current_bright = self.get_brightness()
+                # self.pct.set_brightness(current_bright)
+
+                self.pct.set_brightness(self.dim)
+                # self.pct.set_brightness(1)
+
+
+                full_seconds = full_seconds - 1
+            if not self.stopped or self.skipped:
+                #after a short break, 5 min, we go back to 25 min
+                # self.brightness.set_brightness(90)
+                # current_bright = self.get_brightness()
+                # self.pct.set_brightness(current_bright)
+                self.pct.set_brightness(self.current_b)
+
+                self.tabs.select(0)
+                self.start_timer()
+
+        elif timer_id == 3:
+            # full_seconds = 60 * 15
+            full_seconds = 5
+            while full_seconds > 0 and not self.stopped:
+                minutes, seconds = divmod(full_seconds, 60)
+                self.long_break_timer_label.config(text = f"{minutes:02d}: {seconds:02d}")
+                self.root.update()
+                time.sleep(1)
+
+                # self.pct.set_brightness(1)
+                self.pct.set_brightness(self.dim)
+
+                full_seconds = full_seconds - 1
+            if not self.stopped or self.skipped:
+
+                self.pct.set_brightness(self.current_b)
+
+                self.tabs.select(0)
+                self.start_timer()
+
+        else:
+            print("Invalid timer id")
+
+    def reset_clock(self):
+        self.stopped = True
+        self.skipped = False
+        self.pomodoros = 0
+
+        self.pomodoro_timer_label.config(text="25:00")
+        self.short_break_timer_label.config(text="05:00")
+        self.long_break_timer_label.config(text="15:00")
+        self.pomodoro_counter_label.config(text="Pomodoros: 0")
+
+
+    def skip_clock(self):
+        current_tab = self.tabs.index(self.tabs.select())
+        if current_tab == 0:
+            self.pomodoro_timer_label.config(text="25:00")
+        elif current_tab == 1:
+            self.short_break_timer_label.config(text="05:00")
+        elif current_tab == 2:
+            self.long_break_timer_label.config(text="15:00")
+
+        self.stopped = True
+        self.skipped = True
+
+
+if __name__ == "__main__":
+    pctobj1 = pct
+    current_b = pctobj1.get_brightness()
+    dim = 2
+    Hun(current_b, dim)
